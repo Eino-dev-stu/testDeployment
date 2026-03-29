3693092820e4a439fee198e110ef08ff995ad973
Eino-dev-stu <juhan.pg@gmail.com>
2026-03-29 16:36:02 +0300

    first

---
+++ tests/add.test.js
@@ -0,0 +1,19 @@
+import add from "../src/add.js"
+describe("add.js testit", () => {
+  test("lisää kaksi positiivista lukua oikein", () => {
+    expect(add(6, 4)).toBe(10)
+  })
+
+  test("lisää positiivisen ja negatiivisen luvun", () => {
+    expect(add(6, -4)).toBe(2)
+  })
+
+  test("lisää kaksi negatiivista lukua", () => {
+    expect(add(-6, -4)).toBe(-10)
+  })
+
+  test("palauttaa luvun itse, jos toinen parametri on 0", () => {
+    expect(add(5, 0)).toBe(5)
+    expect(add(0, 5)).toBe(5)
+  })
+})
