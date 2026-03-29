856f0ad6c09da21a2aa58b8823593b3b2b363a78
Eino-dev-stu <juhan.pg@gmail.com>
2026-03-29 19:00:42 +0300

    tests

--- tests/upperFirst.test.js
+++ tests/upperFirst.test.js
@@ -1,7 +1,7 @@
 import upperFirst from "../src/upperFirst"
 describe("upperFirst.js testit", () => {
-  test("muuttaa kokonaan isot kirjaimet oikeaksi (FRED -> Fred)", () => {
-    expect(upperFirst("FRED")).toBe("Fred")
+  test("muuttaa kokonaan isot kirjaimet oikeaksi (FRED -> FRED)", () => {
+    expect(upperFirst("FRED")).toBe("FRED")
   })
   test("muuttaa kokonaan pienet kirjaimet oikeaksi (fred -> Fred)", () => {
     expect(upperFirst("fred")).toBe("Fred")
